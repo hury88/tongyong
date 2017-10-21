8df6ef911a8ba14793d6f1c9c2a33ad72431bbe5
hury <hury88haha@gmail.com>
2017-10-21 17:21:53 +0800

    增加自定义函数

--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -16,4 +16,5 @@ return array(
     '58571171fd5812e6e447dce228f52f4d' => $vendorDir . '/laravel/framework/src/Illuminate/Support/helpers.php',
     '4a1f389d6ce373bda9e57857d3b61c84' => $vendorDir . '/barryvdh/laravel-debugbar/src/helpers.php',
     'f18cc91337d49233e5754e93f3ed9ec3' => $vendorDir . '/laravelcollective/html/src/helpers.php',
+    '1139fa00f9eaf9b6db02e632dc9b50b0' => $baseDir . '/fundation/helpers.php',
 );
